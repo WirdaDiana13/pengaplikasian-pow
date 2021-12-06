3f3afff9a9d99b41be6bb8f46382e10c7ab7add8
wirda diana nesywa <92450175+WirdaDiana13@users.noreply.github.com>
2021-12-06 10:51:34 +0700

    Add files via upload

---
+++ postest 2 ddp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Wirda Diana Nesywa
+//2117051059
+
+int main (){
+	int a;
+	int b;
+	int c;
+	int x;
+    int f;
+	
+	cout<<"Angka = ";
+	cin>>a>>b>>c>>x;
+	f = a*pow(x,2)+b*x+c;
+	cout<<"f adalah "<<f;
+	
+}
+
